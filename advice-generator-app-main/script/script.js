194ee9d01aa2d90bda4da3ec37b5240a8efa259a
Lais Talita Brito de Oliveira <116667293+laisTalita@users.noreply.github.com>
2025-10-29 16:36:03 -0300

    up

--- advice-generator-app-main/script/script.js
+++ advice-generator-app-main/script/script.js
@@ -1,10 +1,20 @@
 let bot = document.getElementById("botao")
+let idEl = document.getElementById("id")
+let textEl = document.getElementById("text")
 
-bot.addEventListener('click',displayText)
-
+async function getAdviceById(id) {
+    const resposta = await fetch(`https://api.adviceslip.com/advice/${id}`);
+    const dados = await resposta.json();
+    idEl.textContent = dados.slip.id;
+    textEl.textContent = `${dados.slip.advice}`;
+}
 async function displayText() {
     const resposta = await fetch(`https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`);
     const dados = await resposta.json()
-    document.getElementById("id").textContent= dados.slip.id
-    document.getElementById("text").textContent=dados.slip.advice
+    idEl.textContent= dados.slip.id
+    textEl.textContent= dados.slip.advice
 }
+
+bot.addEventListener('click',displayText)
+getAdviceById(117)
+
